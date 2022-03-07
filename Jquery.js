53c17828f5285edf93c2314c9bd2c65441fb4097
AnhTu <leanhtu9916@gmail.com>
2022-03-07 13:52:22 +0700

    thêm các js

---
+++ Jquery.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+    $('.contect').slick({
+        Infinity:false,
+    });
+  });
+$(document).ready(function(){
+    $('.list-view').slick({
+        infinite: true,
+        slidesToShow: 2,
+        
+    });
+  });
+$(document).ready(function(){
+    $('.list-view1').slick({
+        infinite: true,
+        slidesToShow: 2,
+        
+    });
+  });
---
+++ scrip_slide.js
@@ -0,0 +1,35 @@
+var slideIndex = 0;
+showSlides();
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides2(slideIndex += n);
+}
+
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("fade");
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+}
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("fade");
+ 
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000);
+}
+
